3b130132bcfd98da63cf66535d58ca64274b3c38
Kirill Begey <kirill.begey@mail.ru>
2024-07-14 15:53:17 +0300

    feat: добавить требуемые лоадеры

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,57 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env) => {
   const isDev = env.mode === 'development';
+  const isProd = env.mode === 'production';
+
+  const htmlLoader = {
+    test: /\.html$/i,
+    loader: 'html-loader',
+  };
+
+  const postCssLoader = {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        plugins: [
+          [
+            'postcss-preset-env',
+            {
+              browsers: 'last 4 versions',
+            },
+          ],
+        ],
+      },
+    },
+  };
+
+  const cssLoader = {
+    test: /\.css$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      'css-loader',
+      isProd && postCssLoader,
+    ],
+  };
+
+  const fontsLoader = {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: 'asset/resource',
+    generator: {
+      filename: 'fonts/[name][ext]',
+    },
+  };
+
+  const svgLoader = {
+    test: /\.svg$/i,
+    type: 'asset/resource',
+    generator: {
+      filename: 'images/[name].[contenthash:8][ext]',
+    },
+  };
 
   return {
     mode: env.mode ?? 'development',
@@ -29,6 +77,18 @@ module.exports = (env) => {
       }),
       new webpack.HotModuleReplacementPlugin(),
       isDev && new webpack.ProgressPlugin(),
-    ],
+      isProd && new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      }),
+    ].filter(Boolean),
+    module: {
+      rules: [
+        isProd && htmlLoader,
+        cssLoader,
+        fontsLoader,
+        svgLoader,
+      ],
+    },
   };
 };
